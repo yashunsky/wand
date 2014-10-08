5b018791127b2467dcf0acf6e62db8d3a70e79f0
Maxim Rytikov <tayamarn@yandex-team.ru>
2014-10-08 11:12:24 +0400

    Mild refactoring

--- proof-of-concept/wand_gui.py
+++ proof-of-concept/wand_gui.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from PySide.QtGui import QApplication, QWidget, QLabel, QTextEdit, QGridLayout, QComboBox
-from PySide.QtCore import QTimer
-from time import time
-import sys
+from copy import deepcopy
+from math import sin, cos
+import os
 import serial
-
-from IMU import IMU
-from Stroke import Stroke
-from Selector import Selector
+import sys
+from time import time
 
 import numpy as np
-from math import sin, cos
+from PySide.QtGui import QApplication, QWidget, QLabel, QTextEdit, QGridLayout, QComboBox
+from PySide.QtCore import QTimer
 
+from IMU import IMU
+from Selector import Selector
+from Stroke import Stroke
 from stroke_widget import StrokeWidget
-
-from copy import deepcopy
-
-from time import time
 
 
 PLATFORM_SPECIFIC_QUOTIENTS = {
     'stm': ((744, -499, -491), (1857, 530, 426)),
     'arduino': ((-504, -615, -564), (597, 488, 384))
 }
+CORE_FILENAME = 'tetra_v2.txt'
+LEARNED_FOLDER = 'learned'
+MAX_DATA_TIMELAPSE = 0.05
 
-class Listner(QWidget):
-    """docstring for Listner"""
+
+class Listener(QWidget):
     def __init__(self, core_file_name):
-        super(Listner, self).__init__()
+        super(Listener, self).__init__()
         self.core_file_name = core_file_name
         self.resize(500, 500)
         self.out = QTextEdit(self)
@@ -38,7 +38,6 @@ class Listner(QWidget):
         self.selector = QComboBox()
         self.grid.addWidget(self.display, 0, 0, 1, 1)
         self.grid.addWidget(self.selector, 1, 0, 1, 1)
-        # self.grid.addWidget(self.out, 2, 0, 1, 1)
         self.serial_timer = QTimer()
         self.serial_timer.setInterval(20)
         self.serial_timer.timeout.connect(self.get_data)
@@ -50,17 +49,17 @@ class Listner(QWidget):
         self.process_timer.start()
 
         port = "/dev/ttyUSB0"
-        self.ser = serial.Serial(port, 115200, timeout=0)
-        self.log = []
+        self.serial = serial.Serial(port, 115200, timeout=0)
+        self.incoming_data = []
 
-        self.im = IMU(PLATFORM_SPECIFIC_QUOTIENTS['stm'])
-        self.st = Stroke()
-        self.sl = Selector(self.core_file_name)
+        self.imu = IMU(PLATFORM_SPECIFIC_QUOTIENTS['stm'])
+        self.stroke = Stroke()
+        self.selector = Selector(self.core_file_name)
 
-        self.st.widget = self.display
-        self.st.on_done = self.get_stroke
+        self.stroke.widget = self.display
+        self.stroke.on_done = self.get_stroke
 
-        self.prev = None
+        self.previous_time = None
 
         self.data_buffer = ''
 
@@ -70,7 +69,7 @@ class Listner(QWidget):
         self.timer.timeout.connect(self.set_background)
 
     def init_selector(self):
-        sel_lines = self.sl.letters_dict.keys()
+        sel_lines = self.selector.letters_dict.keys()
         sel_lines.insert(0, 'free run')
         self.selector.addItems(sel_lines)
         self.selector.currentIndexChanged.connect(self.set_background)
@@ -79,96 +78,71 @@ class Listner(QWidget):
         letter = self.selector.currentText()
         self.display.set_background(self.core_file_name, letter)
 
+    def store_stroke(self, key, stroke):
+        self.display.set_background(self.core_file_name, key, color='g')
+        file_name = '{key}{time}.txt'.format(key=key, time=int(time()))
+        file_path = os.path.join(LEARNED_FOLDER, file_name)
+        np.savetxt(file_path, stroke)
+        self.timer.start(1000)
+
     def get_stroke(self, stroke):
-        #np.savetxt('new_basis/%.0f.txt' % time(), stroke)
-
-        #return
-
-        letters = self.sl.check_stroke(stroke)
-
+        letters = self.selector.check_stroke(stroke)
         letter = self.selector.currentText()
 
-        key = None
-
-        if letter in letters:
-            key = letter
-
         if letter == 'free run' and letters:
-            key = letters[0]
-
-        if key is not None:
-            self.display.set_background(self.core_file_name, key, color='g')
-            np.savetxt('learned/%s%.0f.txt' % (key, time()), stroke)
-            self.timer.start(1000)
+            self.store_stroke(letters[0], stroke)
+        elif letter in letters:
+            self.store_stroke(letter, stroke)
 
     def process(self):
-        local_log = deepcopy(self.log)
-        self.log = []
+        local_data_storage = deepcopy(self.incoming_data)
+        self.incoming_data = []
 
-        data = [0]*10
-        while local_log:
-            data = local_log.pop(0)
-            if self.prev is None:
-                self.prev = data[0]
+        for data in local_data_storage:
+            if self.previous_time is None:
+                self.previous_time = data[0]
                 continue
 
-            data[0], self.prev = data[0]-self.prev, data[0]
+            data[0], self.previous_time = data[0] - self.previous_time, data[0]
 
-            if data[0] < 0.05:
-                self.im.calc(data)
-
+            if data[0] < MAX_DATA_TIMELAPSE:
+                self.imu.calc(data)
                 gyro = np.array([data[7:]])
-
-                Yr = self.im.get_y_direction()
-
-                self.st.set_data(Yr, np.linalg.norm(gyro))
-
+                Yr = self.imu.get_y_direction()
+                self.stroke.set_data(Yr, np.linalg.norm(gyro))
 
     def get_data(self):
         line = ''
-
         try:
-
-            self.data_buffer += self.ser.read(self.ser.inWaiting())
-
+            self.data_buffer += self.serial.read(self.serial.inWaiting())
             if self.data_buffer == '':
                 return
-
+            # Is buffer guarranteed to return data delimited by \r\n?
+            # Can it pass data in some other format?
             breaked = self.data_buffer.split('\r\n')
-
             self.data_buffer = breaked[-1]
 
+            # What does this piece of code do? Should it raise some exception?
             if len(breaked) > 1:
                 line = breaked[-2]
             else:
                 return
 
             result = [float(d) for d in line.split()]
-
-            if not len(result) == 9:
-                return
-
+            if len(result) != 9:
+                raise ValueError('Nine pieces of data should be provided.')
             new_line = [time()] + result
-
-            self.log.append(new_line)
-            # with open('data.raw', 'a') as f:
-            #    f.write(' '.join([str(d) for d in new_line])+'\n')
-
-
+            self.incoming_data.append(new_line)
         except KeyboardInterrupt:
             raise
         except Exception as e:
+            # Why the exceptions are silenced? Shouldn't they be raised?
+            # Which types of exceptions are expected to be caught here?
             print e
-
-
-
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-
-    ls = Listner('tetra_v2.txt')
-
-    ls.show()
-
+    listener = Listener(CORE_FILENAME)
+    listener.show()
     sys.exit(app.exec_())

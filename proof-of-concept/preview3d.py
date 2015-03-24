0ad702819d3b297810860d8f052c3bfa20e5f597
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2015-03-24 12:08:01 +0300

    gradient fill for 3D strokes

--- proof-of-concept/preview3d.py
+++ proof-of-concept/preview3d.py
@@ -17,19 +17,31 @@ def show_strokes_3d(key,strokes, mean_stroke):
     # ygrid = gl.GLGridItem()
     zgrid = gl.GLGridItem()
 
-    color = np.array([0, 0, 1, 0.5])
-    for stroke in strokes:
-        pos = stroke['data']
+    # color = np.array([0, 0, 1, 0.2])
+    # for stroke in strokes:
+    #     pos = stroke['data']
 
-        colors = np.tile(color, (pos.shape[0], 1))
+    #     colors = np.tile(color, (pos.shape[0], 1))
 
-        line = gl.GLLinePlotItem(pos=pos, color=colors, width=5)
-        view.addItem(line)
+    #     line = gl.GLLinePlotItem(pos=pos, color=colors, width=5)
+    #     view.addItem(line)
 
-    color = np.array([1, 0, 0, 1])
+
     pos = mean_stroke[:, :3]
-    colors = np.tile(color, (pos.shape[0], 1))
-    line = gl.GLLinePlotItem(pos=pos, color=colors, width=10)
+
+    color1 = np.array([0, 1, 0, 1])
+    color2 = np.array([1, 0, 0, 1])
+
+    steps = np.linspace(0, 1, pos.shape[0])
+
+    R = np.interp(steps, np.array([0, 1]), np.array([color1[0], color2[0]]))
+    G = np.interp(steps, np.array([0, 1]), np.array([color1[1], color2[1]]))
+    B = np.interp(steps, np.array([0, 1]), np.array([color1[2], color2[2]]))
+    A = np.interp(steps, np.array([0, 1]), np.array([color1[3], color2[3]]))
+
+    colors = np.vstack((R, G, B, A)).T
+
+    line = gl.GLLinePlotItem(pos=pos, color=colors, width=40, mode='line_strip')
     view.addItem(line)
 
     # view.addItem(xgrid)

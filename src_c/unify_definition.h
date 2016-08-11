e037f7e0f885fb41d14c8087cf7ee0147f22c41a
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2016-08-11 23:37:01 +0300

    strore data at buffer's tail

--- src_c/unify_definition.h
+++ src_c/unify_definition.h
@@ -6,7 +6,7 @@
 #define STROKE_MAX_LENGTH 256
 
 float getDist(const float a[DIMENTION], const float b[DIMENTION]);
-void unifyStroke(float stroke[STROKE_MAX_LENGTH][DIMENTION], float newStroke[SEGMENTATION][DIMENTION], int length);
+void unifyStroke(float stroke[STROKE_MAX_LENGTH][DIMENTION], float newStroke[SEGMENTATION][DIMENTION], int start);
 float checkStroke(float stroke[SEGMENTATION][DIMENTION], const float description[SEGMENTATION][DIMENTION + 1]);
 int getStroke(float stroke[STROKE_MAX_LENGTH][DIMENTION], int length, unsigned long access);
 

a55ab7c53e3ce525b76186e655195f7f60e51db9
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2016-08-11 22:48:37 +0300

    change default dimention size

--- src_py/AbstractStrokeSplitter.py
+++ src_py/AbstractStrokeSplitter.py
@@ -40,7 +40,7 @@ class AbstractStrokeSplitter(object):
         self.gyro = gyro
 
         stroke = None
-        dimention = None
+        dimention = 0
         too_short = None
 
         if self.gyro > self.gyro_min:

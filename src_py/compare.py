a2e1e3456b5a69c52948e0c4862c73237745cb44
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2016-08-11 22:48:58 +0300

    fix compare script

--- src_py/compare.py
+++ src_py/compare.py
@@ -1,14 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Selector import Selector
-
 import json
 
-CORE_STROKES_FILE = 'generation.json'
-CORE_STROKES_NAMES = 'stroke_names.json'
+import numpy as np
 
-CELL_WIDTH = 18
+from unify_definition import get_letter
+
+CORE_STROKES_FILE = '../generation.json'
+CORE_STROKES_NAMES = '../stroke_names.json'
+
+CELL_WIDTH = 10
+
+
+class Selector(object):
+    def __init__(self, path):
+
+        self.letters_dict = {}
+
+        with open(path, 'r') as f:
+            data = json.load(f)
+            letters_lists = data['letters']
+            for key, item in letters_lists.items():
+                self.letters_dict[key] = np.array(item)
+            self.segmentation = data['segmentation']
+
+    def check_stroke(self, stroke):
+        letters = get_letter(stroke, self.segmentation,
+                             self.letters_dict)
+        return letters
 
 
 if __name__ == '__main__':

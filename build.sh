404d3be1b16c3da08b8598b3762df01bbe6c8cfe
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2016-09-03 11:46:13 +0300

    compile only requiered files, then restore everithing

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
+cd src_c/extern_sm/
+mv main.c tmp
+mv *desktop* tmp
+mv bsp.h tmp
+cp python/* .
+cd ../../
 gcc -x c++ -dynamiclib -I/usr/local/include/python2.7/ -lpython2.7 src_c/*.cpp src_c/*.h src_c/extern_sm/*.c src_c/extern_sm/*.h
+mv src_c/extern_sm/tmp/* src_c/extern_sm/
+rm src_c/extern_sm/*python*
 mv a.out migration_to_c/c_wrap.so

a47d13a002053a7fec133027d633a6745645b111
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2017-05-24 21:24:29 +0300

    keep same .so in both directories

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ cd ../../
 gcc -x c++ -dynamiclib -I/usr/local/include/python2.7/ -lpython2.7 src_c/*.cpp src_c/*.h src_c/extern_sm/*.c src_c/extern_sm/*.h
 mv src_c/extern_sm/tmp/* src_c/extern_sm/
 rm src_c/extern_sm/*python*.*
-mv a.out migration_to_c/c_wrap.so
+mv a.out src_py/c_wrap.so
+cp src_py/c_wrap.so migration_to_c/c_wrap.so

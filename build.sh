2501b2bd34d82c0d8a2e690686db175f9c9c84e4
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2017-05-31 22:22:07 +0300

    set new build script

--- build.sh
+++ build.sh
@@ -1,11 +1,5 @@
-cd src_c/extern_sm/
-mv main.c tmp
-mv *desktop* tmp
-mv bsp.h tmp
-cp python/* .
-cd ../../
-gcc -x c++ -dynamiclib -I/usr/local/include/python2.7/ -lpython2.7 src_c/*.cpp src_c/*.h src_c/extern_sm/*.c src_c/extern_sm/*.h
-mv src_c/extern_sm/tmp/* src_c/extern_sm/
-rm src_c/extern_sm/*python*.*
+./src_py/export_sources.py $1 python
+gcc -x c++ -dynamiclib -I/usr/local/include/python2.7/ -lpython2.7 output/*.cpp output/*.c output/*.h
 mv a.out src_py/c_wrap.so
-cp src_py/c_wrap.so migration_to_c/c_wrap.so
+rm tmp/*
+./src_py/export_sources.py $1 fw

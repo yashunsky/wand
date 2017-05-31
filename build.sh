288a2ba83e969e0189a4d7512b4b31be31eac039
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2017-06-01 00:54:36 +0300

    store result in output

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+rm -r output
 ./src_py/export_sources.py $1 python
 gcc -x c++ -dynamiclib -I/usr/local/include/python2.7/ -lpython2.7 output/*.cpp output/*.c output/*.h
 mv a.out src_py/c_wrap.so
-rm tmp/*
+rm -r output
 ./src_py/export_sources.py $1 fw

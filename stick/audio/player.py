98f9e5b83e5ae0ffdf0f30cd6e7cf04e9e207c10
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2019-02-24 10:39:11 +0300

    split audio by channels

--- stick/audio/player.py
+++ stick/audio/player.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
 
+from copy import copy
 from time import sleep
 import os
 import pyaudio
@@ -13,42 +14,78 @@ FILE_TEMPLATE = os.path.join(
     '%s.wav')
 
 
+class Side(object):
+    def __init__(self, open_file):
+        super(Side, self).__init__()
+        self.sequence = []
+        self.wf = None
+        self.data = None
+        self.open_file = open_file
+
+    def enqueue(self, key):
+        self.sequence.append(key)
+
+    def get_data(self):
+        if self.sequence and self.wf is None:
+            self.wf = self.open_file(self.sequence.pop(0))
+
+        if self.wf is not None:
+            self.data = self.wf.readframes(CHUNK)
+            if len(self.data) == 0:
+                self.data = None
+                self.wf = None
+
+        return copy(self.data)
+
+
 class Player(object):
     def __init__(self, stop_when_done=False):
         super(Player, self).__init__()
         self.stream = None
         self.p = None
-        self.sequence = []
+        self.format = None
+        self.left_side = Side(self.open_file)
+        self.right_side = Side(self.open_file)
         self.in_loop = True
         self.stop_when_done = stop_when_done
 
     def stream_from_wav(self, wf):
-        format = self.p.get_format_from_width(wf.getsampwidth())
-        return self.p.open(format=format,
+        self.format = self.p.get_format_from_width(wf.getsampwidth())
+        return self.p.open(format=self.format,
                            channels=wf.getnchannels(),
                            rate=wf.getframerate(),
                            output=True)
 
     def enqueue(self, element):
-        self.sequence.append(element)
+        side, key = element
+        (self.left_side if side == 'left' else self.right_side).enqueue(key)
+
+    def mix(self, left, right):
+        if left is None:
+            left = [0] * len(right)
+        if right is None:
+            right = [0] * len(left)
+
+        audio_bytes = self.format * 2 // 8
+
+        return bytes([l if not (id // audio_bytes) % 2 else r
+                      for id, (l, r) in enumerate(zip(left, right))])
+
+    def open_file(self, filename):
+        wf = wave.open(FILE_TEMPLATE % filename, 'rb')
+        if self.stream is None:
+            self.stream = self.stream_from_wav(wf)
+        return wf
 
     def play(self):
         try:
             self.p = pyaudio.PyAudio()
             while self.in_loop:
-                if self.sequence:
-                    side, filename = self.sequence.pop(0)
+                left = self.left_side.get_data()
+                right = self.right_side.get_data()
 
-                    wf = wave.open(FILE_TEMPLATE % filename, 'rb')
-
-                    if self.stream is None:
-                        stream = self.stream_from_wav(wf)
-
-                    data = wf.readframes(CHUNK)
-
-                    while len(data) > 0 and self.in_loop:
-                        stream.write(data)
-                        data = wf.readframes(CHUNK)
+                if left is not None or right is not None:
+                    self.stream.write(self.mix(left, right))
 
                 else:
                     if self.stop_when_done:

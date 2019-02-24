eda371c19e0e81ff14a292c357cebc70e89a5ff7
Vladimir Yashunsky <vladimir.yashunsky@gmail.com>
2019-02-24 15:32:33 +0300

    restore callbacks order for correct sound messages

--- stick/duellist.py
+++ stick/duellist.py
@@ -53,8 +53,8 @@ class Duellist(object):
         if self.catched_spells:
             top_spell = self.catched_spells[0]
             if spell in top_spell.shields:
-                self.remove_top_spell()
                 self.on_defence_succeded(top_spell)
+                self.remove_top_spell()
                 return
 
         if spell.is_attack and self.adversary is not None:
@@ -79,8 +79,8 @@ class Duellist(object):
             self.is_defending = self.sequence in expexted_prefixes
             if not self.is_defending and self.timeout < 0:
                 top_spell = self.catched_spells[0]
-                self.remove_top_spell()
                 self.on_defence_failed(top_spell)
+                self.remove_top_spell()
 
     def for_gui(self):
         if self.catched_spells and not self.is_defending:
